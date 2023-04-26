b816342c0fe4307e9f62d887619dc78d005550d0
LauraATamari <99518185+LauraATamari@users.noreply.github.com>
2023-04-25 21:08:40 -0300

    Update checkpoint2.py

--- checkpoint2.py
+++ checkpoint2.py
@@ -6,19 +6,20 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-#logica para identificar se pedra, papel e tesoura
+#logica para identificar se pedra, papel e tesoura da mão esquerda
 def getHandMoveLeft(hand_landmarks):
     landmarks = hand_landmarks.landmark
     if (landmarks[5].x > landmarks[8].x) and (landmarks[9].x > landmarks[12].x) and (landmarks[13].x > landmarks[16].x) and (landmarks[17].x > landmarks[20].x): return "pedra"
     elif (landmarks[5].x < landmarks[8].x) and (landmarks[9].x < landmarks[12].x) and (landmarks[13].x < landmarks[16].x) and (landmarks[17].x < landmarks[20].x): return "papel"
     else: return "tesoura"
-
+#logica para identificar se pedra, papel e tesoura da mão direita
 def getHandMoveRight(hand_landmarks):
     landmarks = hand_landmarks.landmark
     if (landmarks[5].x < landmarks[8].x) and (landmarks[9].x < landmarks[12].x) and (landmarks[13].x < landmarks[16].x) and (landmarks[17].x < landmarks[20].x): return "pedra"
     elif (landmarks[5].x > landmarks[8].x) and (landmarks[9].x > landmarks[12].x) and (landmarks[13].x > landmarks[16].x) and (landmarks[17].x > landmarks[20].x): return "papel"
     else: return "tesoura"
 
+#iniciando em 0/nada escrito
 gameText = ""
 left_score = 0
 right_score = 0
@@ -26,8 +27,6 @@ time = 0
 
 #captura do video
 cap = cv2.VideoCapture('pedra-papel-tesoura.mp4')
-
-#gameText, string que vai mostrar quem venceu
 
 #configuracoes para identificar as maos
 with mp_hands.Hands(
@@ -37,12 +36,13 @@ with mp_hands.Hands(
 
     while True:
         success, img = cap.read()
-
+        #revertendo imagem em BGR para identificar melhor
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
+        #resultado do mediapipe 
         results = hands.process(img)
-
+        #revertendo imagem para cor normal/RGB
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        #desenhando os pontos nas mãos
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
@@ -52,7 +52,7 @@ with mp_hands.Hands(
                     mp_drawing_styles.get_default_hand_landmarks_style(),
                     mp_drawing_styles.get_default_hand_connections_style()
                 )
-        
+                
         hls = results.multi_hand_landmarks
         #se dentro dos resuldados tiver 2 maos
         if hls and len(hls) == 2:
@@ -79,7 +79,7 @@ with mp_hands.Hands(
                     print("Not identified")
             else:
                 success = False
-                
+            #condições para add pontuação
             time = (time + 1 ) % 180
             time += 1
             if time >= 170 and time <= 170.5:
@@ -105,7 +105,7 @@ with mp_hands.Hands(
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
-
+        #exibindo pontuação, e quem ganha no final
         print("PONTUACAO")
         print("Jogador 1: ", left_score, " pontos")
         print("Jogador 2: ", right_score, " pontos")
